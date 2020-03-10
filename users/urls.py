dd4bac7e44110f0c656032675c4382578ce2f61b
Leonardo Nava (L3oNav) <L3oNav@outlook.com>
2020-03-10 16:22:26 -0700

    hot fix logout view

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.LoginView.as_view(), name="login"),
     path('<str:username>/detail/', views.UserDetailView.as_view(), name='detail'),
     path('signup/', views.SignUpView.as_view(), name="signup"),
-    path('logout/', views.LogoutView.as_view(), name="logout"),
-    path('update_profile/', views.LogoutView, name="update_profile")
+    path('logout/', views.LogoutView, name="logout"),
+    path('update_profile/', views.UpdateProfileView.as_view(), name="update_profile")
 ]
